9daaaf6326701ca5ff70845e18b62a77cef835d7
Nick Castermans <23052226+ncastermans@users.noreply.github.com>
2024-03-12 17:20:57 +0100

    defined item visitor following visitor pattern guidelines

---
+++ TypeScript/app/visitors/item-visitor.ts
@@ -0,0 +1,8 @@
+import { Item } from '../models/item';
+
+export interface ItemVisitor {
+  visitBaseItem(item: Item): void;
+  visitAgedBrie(item: Item): void;
+  visitBackstagePass(item: Item): void;
+  visitLegendaryItem(item: Item): void;
+}
