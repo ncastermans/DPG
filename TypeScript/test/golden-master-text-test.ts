155a2b7edb19cfcbd55ba2b942efc2756b9e2403
Nick Castermans <23052226+ncastermans@users.noreply.github.com>
2024-03-12 22:23:28 +0100

    added snapshot test with jest

--- TypeScript/test/golden-master-text-test.ts
+++ TypeScript/test/golden-master-text-test.ts
@@ -1,4 +1,5 @@
-import { Item, GildedRose } from '../app/gilded-rose';
+import { GildedRose } from '@/gilded-rose';
+import { Item } from '@/models/item';
 
 console.log('OMGHAI!');
 
--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,10 +1,31 @@
 import { GildedRose } from '@/gilded-rose';
 import { Item } from '@/models/item';
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+describe('GildedRose', () => {
+  it('should match snapshot', () => {
+    console.log = jest.fn(); // Mock console.log to prevent output during test
+
+    const items = [
+      new Item('+5 Dexterity Vest', 10, 20),
+      new Item('Aged Brie', 2, 0),
+      new Item('Elixir of the Mongoose', 5, 7),
+      new Item('Sulfuras, Hand of Ragnaros', 0, 80),
+      new Item('Sulfuras, Hand of Ragnaros', -1, 80),
+      new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
+      new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
+      new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
+      new Item('Conjured Mana Cake', 3, 6),
+    ];
+
+    const gildedRose = new GildedRose(items);
+
+    let days = 30;
+
+    for (let i = 0; i < days + 1; i++) {
+      gildedRose.updateQuality();
+    }
+
+    // Compare the console.log output with the snapshot
+    expect(console.log).toMatchSnapshot();
   });
 });
