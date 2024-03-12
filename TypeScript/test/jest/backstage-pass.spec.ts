2138b0740dcb1fad94060a89627bfda0ccb9a523
Nick Castermans <23052226+ncastermans@users.noreply.github.com>
2024-03-12 17:05:01 +0100

    added test cases for legendary item

--- TypeScript/test/jest/backstage-pass.spec.ts
+++ TypeScript/test/jest/backstage-pass.spec.ts
@@ -1,74 +1,73 @@
-import { GildedRose, Item } from "@/gilded-rose";
+import {GildedRose, Item} from '@/gilded-rose';
 
 describe('Backstage passes to a TAFKAL80ETC concert', () => {
-        it('should increase quality by 2 when there are 10 days or less', () => {
-            const itemsMock: Item[] = [
-                new Item('Backstage passes to a TAFKAL80ETC concert', 10, 10)
-            ];
-            const app:GildedRose = new GildedRose(itemsMock);
-            app.updateQuality();
-            expect(app.items[0].quality).toEqual(12);
-            expect(app.items[0].sellIn).toEqual(9);
-            app.updateQuality();
-            expect(app.items[0].quality).toEqual(14);
-            expect(app.items[0].sellIn).toEqual(8);
-        });
+  it('should increase quality by 2 when there are 10 days or less', () => {
+    const itemsMock: Item[] = [
+      new Item('Backstage passes to a TAFKAL80ETC concert', 10, 10),
+    ];
+    const app: GildedRose = new GildedRose(itemsMock);
+    app.updateQuality();
+    expect(app.items[0].quality).toEqual(12);
+    expect(app.items[0].sellIn).toEqual(9);
+    app.updateQuality();
+    expect(app.items[0].quality).toEqual(14);
+    expect(app.items[0].sellIn).toEqual(8);
+  });
 
-        it('should increase quality by 3 when there are 5 days or less', () => {
-            const itemsMock: Item[] = [
-                new Item('Backstage passes to a TAFKAL80ETC concert', 5, 10)
-            ];
-            const app:GildedRose = new GildedRose(itemsMock);
-            app.updateQuality();
-            expect(app.items[0].quality).toEqual(13);
-            expect(app.items[0].sellIn).toEqual(4);
-            app.updateQuality();
-            expect(app.items[0].quality).toEqual(16);
-            expect(app.items[0].sellIn).toEqual(3);
-        });
+  it('should increase quality by 3 when there are 5 days or less', () => {
+    const itemsMock: Item[] = [
+      new Item('Backstage passes to a TAFKAL80ETC concert', 5, 10),
+    ];
+    const app: GildedRose = new GildedRose(itemsMock);
+    app.updateQuality();
+    expect(app.items[0].quality).toEqual(13);
+    expect(app.items[0].sellIn).toEqual(4);
+    app.updateQuality();
+    expect(app.items[0].quality).toEqual(16);
+    expect(app.items[0].sellIn).toEqual(3);
+  });
 
+  it('should increase quality by 1 when there are more than 10 days', () => {
+    const itemsMock: Item[] = [
+      new Item('Backstage passes to a TAFKAL80ETC concert', 13, 10),
+    ];
+    const app: GildedRose = new GildedRose(itemsMock);
+    app.updateQuality();
+    expect(app.items[0].quality).toEqual(11);
+    expect(app.items[0].sellIn).toEqual(12);
+    app.updateQuality();
+    expect(app.items[0].quality).toEqual(12);
+    expect(app.items[0].sellIn).toEqual(11);
+  });
 
-        it('should increase quality by 1 when there are more than 10 days', () => {
-            const itemsMock: Item[] = [
-                new Item('Backstage passes to a TAFKAL80ETC concert', 13, 10)
-            ];
-            const app:GildedRose = new GildedRose(itemsMock);
-            app.updateQuality();
-            expect(app.items[0].quality).toEqual(11);
-            expect(app.items[0].sellIn).toEqual(12);
-            app.updateQuality();
-            expect(app.items[0].quality).toEqual(12);
-            expect(app.items[0].sellIn).toEqual(11);
-        });
+  it('should decrease quality to 0 when the event has passed', () => {
+    const itemsMock: Item[] = [
+      new Item('Backstage passes to a TAFKAL80ETC concert', -1, 10),
+    ];
+    const app: GildedRose = new GildedRose(itemsMock);
+    app.updateQuality();
+    expect(app.items[0].quality).toEqual(0);
+    expect(app.items[0].sellIn).toEqual(-2);
+  });
 
-        it('should decrease quality to 0 when the event has passed', () => {
-            const itemsMock: Item[] = [
-                new Item('Backstage passes to a TAFKAL80ETC concert', -1, 10)
-            ];
-            const app:GildedRose = new GildedRose(itemsMock);
-            app.updateQuality();
-            expect(app.items[0].quality).toEqual(0);
-            expect(app.items[0].sellIn).toEqual(-2);
-        });
+  it('quality of item can not be negative', () => {
+    const itemsMock: Item[] = [
+      new Item('Backstage passes to a TAFKAL80ETC concert', 20, -1),
+    ];
+    const app: GildedRose = new GildedRose(itemsMock);
+    app.updateQuality();
+    expect(app.items[0].quality).toEqual(0);
+    expect(app.items[0].sellIn).toEqual(19);
+  });
 
-        it('quality of item can not be negative', () => {
-            const itemsMock: Item[] = [
-                new Item('Backstage passes to a TAFKAL80ETC concert', 20, -1)
-            ];
-            const app:GildedRose = new GildedRose(itemsMock);
-            app.updateQuality();
-            expect(app.items[0].quality).toEqual(0);
-            expect(app.items[0].sellIn).toEqual(19);
-        });
-
-        it('quality of item can not be higher than 50', () => {
-            const itemsMock: Item[] = [
-                new Item('Backstage passes to a TAFKAL80ETC concert', 2, 51),
-                new Item('Backstage passes to a TAFKAL80ETC concert', 2, 999)
-            ];
-            const app:GildedRose = new GildedRose(itemsMock);
-            app.updateQuality();
-            expect(app.items[0].quality).toEqual(50);
-            expect(app.items[1].quality).toEqual(50);
-        });
+  it('quality of item can not be higher than 50', () => {
+    const itemsMock: Item[] = [
+      new Item('Backstage passes to a TAFKAL80ETC concert', 2, 51),
+      new Item('Backstage passes to a TAFKAL80ETC concert', 2, 999),
+    ];
+    const app: GildedRose = new GildedRose(itemsMock);
+    app.updateQuality();
+    expect(app.items[0].quality).toEqual(50);
+    expect(app.items[1].quality).toEqual(50);
+  });
 });
---
+++ TypeScript/test/jest/legendary-item.spec.ts
@@ -0,0 +1,25 @@
+import {Item, GildedRose} from '@/gilded-rose';
+
+describe('Legendary Item', () => {
+  it('should not lower sellIn and quality at the end of the day', () => {
+    const itemsMock: Item[] = [new Item('Sulfuras, Hand of Ragnaros', 10, 80)];
+    const app: GildedRose = new GildedRose(itemsMock);
+    app.updateQuality();
+    expect(app.items[0].quality).toEqual(80);
+    expect(app.items[0].sellIn).toEqual(10);
+    app.updateQuality();
+    expect(app.items[0].quality).toEqual(80);
+    expect(app.items[0].sellIn).toEqual(10);
+  });
+
+  it('quality should always be 80', () => {
+    const itemsMock: Item[] = [new Item('Sulfuras, Hand of Ragnaros', -1, 10)];
+    const app: GildedRose = new GildedRose(itemsMock);
+    app.updateQuality();
+    expect(app.items[0].quality).toEqual(80);
+    expect(app.items[0].sellIn).toEqual(-1);
+    app.updateQuality();
+    expect(app.items[0].quality).toEqual(80);
+    expect(app.items[0].sellIn).toEqual(-1);
+  });
+});
