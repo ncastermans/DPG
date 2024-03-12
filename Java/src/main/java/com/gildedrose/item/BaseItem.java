056c3466849f01e5dcea571ac4eb7fc5f3670aeb
Nick Castermans <23052226+ncastermans@users.noreply.github.com>
2024-03-13 00:01:20 +0100

    defined base class that can be used for inheritance

---
+++ Java/src/main/java/com/gildedrose/item/BaseItem.java
@@ -0,0 +1,40 @@
+package com.gildedrose.item;
+
+public class BaseItem {
+    protected final int MAX_QUALITY = 50;
+    protected final int MIN_QUALITY = 0;
+    protected final int SELL_IN_DELTA = 1;
+    protected final int QUALITY_DELTA = 1;
+
+    protected Item item;
+
+    public BaseItem(Item item) {
+        this.item = item;
+    }
+
+    public Item update() {
+        updateQuality();
+        updateSellIn();
+        validateQuality();
+        return item;
+    }
+
+    protected void updateQuality() {
+        int qualityDelta = item.sellIn > 0 ? QUALITY_DELTA : QUALITY_DELTA * 2;
+        item.quality = item.quality - qualityDelta;
+    }
+
+    protected void updateSellIn() {
+        item.sellIn = item.sellIn - SELL_IN_DELTA;
+    }
+
+    protected void validateQuality() {
+        if (item.quality > MAX_QUALITY) {
+            item.quality = MAX_QUALITY;
+        }
+
+        if (item.quality < MIN_QUALITY) {
+            item.quality = MIN_QUALITY;
+        }
+    }
+}
