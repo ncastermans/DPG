88c59bb1195fd89a5136ea2fdd5e0416ad0ee253
Nick Castermans <23052226+ncastermans@users.noreply.github.com>
2024-03-12 17:02:18 +0100

    added test cases for base item

---
+++ TypeScript/test/jest/base-item.spec.ts
@@ -0,0 +1,47 @@
+import {GildedRose, Item} from '@/gilded-rose';
+
+describe('Base Item', () => {
+  it('should lower sellIn and quality at the end of the day', () => {
+    const itemsMock: Item[] = [new Item('Base Item', 10, 10)];
+    const app: GildedRose = new GildedRose(itemsMock);
+    app.updateQuality();
+    expect(app.items[0].quality).toEqual(9);
+    expect(app.items[0].sellIn).toEqual(9);
+    app.updateQuality();
+    expect(app.items[0].quality).toEqual(8);
+    expect(app.items[0].sellIn).toEqual(8);
+  });
+
+  it('should lower quality twice as fast when sellIn < 0', () => {
+    const itemsMock: Item[] = [new Item('Base Item', -1, 10)];
+    const app: GildedRose = new GildedRose(itemsMock);
+    app.updateQuality();
+    expect(app.items[0].quality).toEqual(8);
+    expect(app.items[0].sellIn).toEqual(-2);
+    app.updateQuality();
+    expect(app.items[0].quality).toEqual(6);
+    expect(app.items[0].sellIn).toEqual(-3);
+  });
+
+  it('quality of item can not be negative', () => {
+    const itemsMock: Item[] = [new Item('Base Item', 2, 0)];
+    const app: GildedRose = new GildedRose(itemsMock);
+    app.updateQuality();
+    expect(app.items[0].quality).toEqual(0);
+    expect(app.items[0].sellIn).toEqual(1);
+    app.updateQuality();
+    expect(app.items[0].quality).toEqual(0);
+    expect(app.items[0].sellIn).toEqual(0);
+  });
+
+  it('quality of item can not be higher than 50', () => {
+    const itemsMock: Item[] = [
+      new Item('Base Item', 2, 51),
+      new Item('Base Item 2', 2, 999),
+    ];
+    const app: GildedRose = new GildedRose(itemsMock);
+    app.updateQuality();
+    expect(app.items[0].quality).toEqual(50);
+    expect(app.items[1].quality).toEqual(50);
+  });
+});
