c548046d2687564971becb55a3f2245b7e9567d5
Nick Castermans <23052226+ncastermans@users.noreply.github.com>
2024-03-14 00:10:28 +0100

    extracted magic strings

--- Java/src/main/java/com/gildedrose/item/BaseItem.java
+++ Java/src/main/java/com/gildedrose/item/BaseItem.java
@@ -1,7 +1,7 @@
 package com.gildedrose.item;
 
 public class BaseItem {
-    protected int MAX_QUALITY = 50;
+    protected final int MAX_QUALITY = 50;
     protected final int MIN_QUALITY = 0;
     protected final int SELL_IN_DELTA = 1;
     protected final int QUALITY_DELTA = 1;
@@ -20,20 +20,14 @@ public class BaseItem {
 
     protected void updateQuality() {
         int qualityDelta = item.sellIn >= 0 ? QUALITY_DELTA : QUALITY_DELTA * 2;
-        item.quality = item.quality - qualityDelta;
+        item.quality = Math.min(Math.max(item.quality - qualityDelta, MIN_QUALITY), MAX_QUALITY);
     }
 
     protected void updateSellIn() {
-        item.sellIn = item.sellIn - SELL_IN_DELTA;
+        item.sellIn -= SELL_IN_DELTA;
     }
 
     protected void validateQuality() {
-        if (item.quality > MAX_QUALITY) {
-            item.quality = MAX_QUALITY;
-        }
-
-        if (item.quality < MIN_QUALITY) {
-            item.quality = MIN_QUALITY;
-        }
+        item.quality = Math.min(Math.max(item.quality, MIN_QUALITY), MAX_QUALITY);
     }
 }
